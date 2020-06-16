682e6ff8e8697cdfc7004122299a367b32fe823c
wasw2123 <wasw2123@gmail.com>
2020-06-17 01:05:56 +0900

    content_arrow position

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 "use strict";
 
 // search
-const searchButton = document.querySelectorAll("#headerRight label");
-const searchBar = document.querySelector("#searchArea");
+{
+  const searchButton = document.querySelectorAll("#headerRight label");
+  const searchBar = document.querySelector("#searchArea");
 
-searchButton[1].addEventListener("click", function () {
-  searchBar.style.display = "block";
-  searchButton[1].style.display = "none";
-});
+  searchButton[1].addEventListener("click", function () {
+    searchBar.style.display = "block";
+    searchButton[1].style.display = "none";
+  });
 
-searchBar.addEventListener("focusout", function () {
-  searchBar.style.display = "none";
-  searchButton[1].style.display = "inline";
-});
+  searchBar.addEventListener("focusout", function () {
+    searchBar.style.display = "none";
+    searchButton[1].style.display = "inline";
+  });
+}
