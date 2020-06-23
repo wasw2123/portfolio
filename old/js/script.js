91413e77b5ad4741ac1e77dd25b26b62355f3f4c
wasw2123 <wasw2123@gmail.com>
2020-06-23 10:52:21 +0900

    change project

--- old/js/script.js
+++ old/js/script.js
@@ -17,6 +17,7 @@
 }
 
 // slide
+const contents = document.getElementById("contents");
 const contentsWrap = document.querySelectorAll(".contentsWrap");
 const slideWrap = document.querySelectorAll(".slideWrap");
 const slideList = document.querySelectorAll(".slideList");
@@ -26,7 +27,7 @@ const provBtn = document.querySelector(".provBtn");
 const nextBtn = document.querySelector(".nextBtn");
 let slideHeight = 0;
 let currentIdx = 0;
-const slideSpeed = 300;
+const slideSpeed = 200;
 
 for (let i = 0; i < slideContent.length; i++) {
   // slideContent[i].style.left = i * slideContent.length * contentWidth + "px";
@@ -45,13 +46,20 @@ function calcHeight() {
         slideContent[i].offsetHeight;
     }
   }
-  contentsWrap[0].style.height = slideHeight + "px";
+
+  for (let i = 0; i < contentsWrap.length; i++) {
+    contentsWrap[i].style.height = slideHeight + "px";
+  }
+  contents.style.height = slideHeight * contentsWrap.length + "px";
 }
 calcHeight();
 
-function Sliding(curidx) {
-  slideWrap[0].style.transition = slideSpeed + "ms ease";
-  slideWrap[0].style.left = "-" + curidx * contentWidth + "px";
+function Sliding(curidx, i) {
+  slideWrap[i].style.transition = slideSpeed + "ms";
+  slideWrap[i].style.transform = `translate3d(-${
+    curidx * contentWidth
+  }px, 0px, 0px)`;
+
   currentIdx = curidx;
   console.log(slideWrap[0].style.left);
 
@@ -65,7 +73,7 @@ function idxUpDating() {
     provBtn.classList.remove("disabled");
   }
 
-  if (currentIdx >= slideContent.length - 3) {
+  if (currentIdx >= slideContent.length - 4) {
     nextBtn.classList.add("disabled");
   } else {
     nextBtn.classList.remove("disabled");
@@ -73,11 +81,13 @@ function idxUpDating() {
 }
 
 nextBtn.addEventListener("click", function () {
-  Sliding(currentIdx + 2);
+  Sliding(currentIdx + 3);
 });
 
 provBtn.addEventListener("click", function () {
-  Sliding(currentIdx - 2);
+  Sliding(currentIdx - 3);
 });
 
-Sliding(0);
+for (let i = 0; i < contentsWrap.length; i++) {
+  Sliding(0, i);
+}
